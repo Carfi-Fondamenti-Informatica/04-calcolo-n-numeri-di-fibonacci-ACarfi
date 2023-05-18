cf4b971ea9aafdc6b69b345b3442ea2c02d4c77a
ACarfi <carfi.alessandro@gmail.com>
2023-05-18 15:35:09 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,18 @@
 using namespace std;
 
 int main() {
-   return 0;
+    int n = 0, a = 1, b = 1;
+    cin >> n;
+    if(n<2){
+        cout << "errore" <<endl;
+    }else {
+        cout << "1" << endl;
+        for (int i = 1; i < n; i++) {
+            cout << a << endl;
+            int c = a;
+            a = a + b;
+            b = c;
+        }
+    }
+    return 0;
 }
